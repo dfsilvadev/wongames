649b501340d758c110a9b9763f73e775c695ddf2
Daniel Silva <dani3l.dxp@gmail.com>
2022-01-20 18:53:17 -0300

    feat: definindo os estilos do button

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,55 @@
+import styled, { css, DefaultTheme } from "styled-components";
+
+import { ButtonProps } from ".";
+
+type WrapperProps = {
+  hasIcon: boolean;
+} & Pick<ButtonProps, "size" | "fullWidth">;
+
+const wrapperModifiers = {
+  small: (theme: DefaultTheme) => css`
+    height: 3rem;
+    font-size: ${theme.font.sizes.xsmall};
+  `,
+  medium: (theme: DefaultTheme) => css`
+    height: 4rem;
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
+    font-size: ${theme.font.sizes.small};
+  `,
+  large: (theme: DefaultTheme) => css`
+    height: 5rem;
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+    font-size: ${theme.font.sizes.medium};
+  `,
+  fullWidth: () => css`
+    width: 100%;
+  `,
+  withIcon: (theme: DefaultTheme) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      width: 1.5rem;
+
+      & + span {
+        margin-left: ${theme.spacings.xxsmall};
+      }
+    }
+  `
+};
+
+export const Wrapper = styled.button<WrapperProps>`
+  ${({ theme, size, fullWidth, hasIcon }) => css`
+    background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
+    color: ${theme.colors.white};
+    border: 0;
+    border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xxsmall};
+    cursor: pointer;
+
+    ${!!size && wrapperModifiers[size](theme)}
+    ${!!fullWidth && wrapperModifiers.fullWidth()}
+    ${!!hasIcon && wrapperModifiers.withIcon(theme)}
+  `}
+`;
