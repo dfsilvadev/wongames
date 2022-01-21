ce79d55aa7cc5efa15fbb1600273aff3945c84af
Daniel Silva <dani3l.dxp@gmail.com>
2022-01-21 13:08:37 -0300

    feat: configurando o absolute path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,14 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/components/**/stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"]
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, "../src")
+    ];
+
+    return config;
+  }
 };
