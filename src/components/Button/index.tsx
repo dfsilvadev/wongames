4ad9d5fcfef25b68a28ad91cffd7eb8048ee338b
Daniel Silva <dani3l.dxp@gmail.com>
2022-01-26 10:00:09 -0300

    feat: ajustando o button para aceitar poliformismo "as"

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,22 @@
-import { MouseEvent, ReactNode } from "react";
+import {
+  AnchorHTMLAttributes,
+  ButtonHTMLAttributes,
+  ElementType,
+  ReactNode
+} from "react";
 
 import * as S from "./styles";
 
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>;
+
 export type ButtonProps = {
-  children?: ReactNode;
   size?: "small" | "medium" | "large";
   fullWidth?: boolean;
   icon?: ReactNode;
-  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
-};
+  as?: ElementType;
+} & ButtonTypes;
 
 const Button = ({
   children,
