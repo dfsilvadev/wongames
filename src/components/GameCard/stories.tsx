b6a1ff08749650949a06631bb4a5e8caed2c3969
Daniel Silva <dani3l.dxp@gmail.com>
2022-01-28 15:09:46 -0300

    feat: escrevendo as stories do component gameCard

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,57 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { ComponentProps } from "react";
+
+import GameCard from ".";
+
+export default {
+  title: "GameCard",
+  component: GameCard,
+  args: {
+    title: "Population Zero",
+    developer: "Gearbox Software",
+    img: "https://source.unsplash.com/user/willianjusten/300x140",
+    price: "$215,00",
+    favorite: false
+  },
+  argTypes: {
+    onFav: {
+      action: "Clicked"
+    }
+  },
+  parameters: {
+    layout: "centered"
+  }
+} as Meta;
+
+export const Default: Story<ComponentProps<typeof GameCard>> = ({
+  ...props
+}) => (
+  <div style={{ width: "30rem" }}>
+    <GameCard {...props} />
+  </div>
+);
+
+export const WithRibbon: Story<ComponentProps<typeof GameCard>> = ({
+  ...props
+}) => (
+  <div style={{ width: "30rem" }}>
+    <GameCard {...props} />
+  </div>
+);
+
+WithRibbon.args = {
+  ribbon: "20% OFF",
+  ribbonSize: "small",
+  ribbonColor: "primary"
+};
+
+WithRibbon.argTypes = {
+  ribbonSize: {
+    options: ["small", "normal", "large"],
+    control: { type: "select" }
+  },
+  ribbonColor: {
+    options: ["primary", "secondary"],
+    control: { type: "select" }
+  }
+};
