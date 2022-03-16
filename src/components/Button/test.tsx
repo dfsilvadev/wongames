a46cff5d5c1b3158f310e78e1f5e66cd81e04145
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-16 17:28:39 -0300

    feat: removendo o debug

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -70,13 +70,11 @@ describe("<Button />", () => {
   });
 
   it("should render button as a link", () => {
-    const { debug, container } = renderWithTheme(
+    renderWithTheme(
       <Button as="a" href="/link">
         Buy now
       </Button>
     );
-
-    debug(container);
 
     expect(screen.getByRole("link", { name: /buy now/i })).toHaveAttribute(
       "href",
