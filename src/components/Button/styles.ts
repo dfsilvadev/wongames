6972e8462562ca268d76628f9a9dd1b7c962bf4d
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-16 15:29:08 -0300

    feat: estilos do button minimal

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,10 +1,11 @@
 import styled, { css, DefaultTheme } from "styled-components";
+import { darken } from "polished";
 
 import { ButtonProps } from ".";
 
 type WrapperProps = {
   hasIcon: boolean;
-} & Pick<ButtonProps, "size" | "fullWidth">;
+} & Pick<ButtonProps, "size" | "fullWidth" | "minimal">;
 
 const wrapperModifiers = {
   small: (theme: DefaultTheme) => css`
@@ -32,11 +33,19 @@ const wrapperModifiers = {
         margin-left: ${theme.spacings.xxsmall};
       }
     }
+  `,
+  minimal: (theme: DefaultTheme) => css`
+    background: none;
+    color: ${theme.colors.primary};
+
+    &:hover {
+      color: ${darken(0.1, theme.colors.primary)};
+    }
   `
 };
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth, hasIcon }) => css`
+  ${({ theme, size, fullWidth, minimal, hasIcon }) => css`
     display: inline-flex;
     align-items: center;
     justify-content: center;
@@ -50,10 +59,13 @@ export const Wrapper = styled.button<WrapperProps>`
 
     ${!!size && wrapperModifiers[size](theme)}
     ${!!fullWidth && wrapperModifiers.fullWidth()}
+    ${!!minimal && wrapperModifiers.minimal(theme)}
     ${!!hasIcon && wrapperModifiers.withIcon(theme)}
 
     &:hover {
-      background: linear-gradient(180deg, #e35565 0%, #d958a6 50%);
+      background: ${minimal
+        ? "none"
+        : `linear-gradient(180deg, #e35565 0%, #d958a6 50%)`};
     }
   `}
 `;
