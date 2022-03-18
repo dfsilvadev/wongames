127de238ca4beefebe8015f2540beacb0e5153ab
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-18 17:43:58 -0300

    feat: escrevendo as stories do component radio

---
+++ src/components/Radio/stories.tsx
@@ -0,0 +1,61 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { ComponentProps } from "react";
+
+import Radio from ".";
+
+export default {
+  title: "Radio",
+  component: Radio,
+  argTypes: {
+    labelColor: {
+      options: ["white", "black"],
+      control: { type: "select" }
+    },
+    onCheck: { action: "check" }
+  },
+  args: {
+    labelColor: "white"
+  },
+  parameters: {
+    layout: "centered",
+    backgrounds: {
+      default: "won-dark"
+    }
+  }
+} as Meta;
+
+export const Basic: Story<ComponentProps<typeof Radio>> = ({ ...props }) => (
+  <>
+    <div style={{ padding: 10 }}>
+      <Radio
+        label="primeiro"
+        labelFor="primeiro"
+        id="primeiro"
+        name="nome"
+        value="primeiro"
+        defaultChecked
+        {...props}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Radio
+        label="segundo"
+        labelFor="segundo"
+        id="segundo"
+        name="nome"
+        value="segundo"
+        {...props}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Radio
+        label="terceiro"
+        labelFor="terceiro"
+        id="terceiro"
+        name="nome"
+        value="terceiro"
+        {...props}
+      />
+    </div>
+  </>
+);
