e4c056df9b3775214e0a1b29e3d7a6f1a06f1490
Daniel Silva <dani3l.dxp@gmail.com>
2022-03-24 15:03:00 -0300

    feat: add link entre páginas

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,5 +1,8 @@
+import Link from "next/link";
+
 import Heading from "components/Heading";
 import Logo from "components/Logo";
+
 import { ReactNode } from "react";
 
 import * as S from "./styles";
@@ -14,7 +17,11 @@ const Auth = ({ title, children }: AuthProps) => {
     <S.Wrapper>
       <S.BannerBlock>
         <S.BannerContent>
-          <Logo id="banner" />
+          <Link href="/">
+            <a>
+              <Logo id="banner" />
+            </a>
+          </Link>
 
           <div>
             <Heading size="huge">All your favorite games in one place</Heading>
@@ -31,7 +38,11 @@ const Auth = ({ title, children }: AuthProps) => {
 
       <S.Content>
         <S.ContentWrapper>
-          <Logo color="black" size="large" id="form" />
+          <Link href="/">
+            <a>
+              <Logo color="black" size="large" id="form" />
+            </a>
+          </Link>
 
           <Heading color="black" lineColor="secondary" lineLeft>
             {title}
