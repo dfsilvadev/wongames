2198514049026bfa7b1bd59d39de6dcb903531b7
Daniel Silva <dani3l.dxp@gmail.com>
2022-04-04 13:04:56 -0300

    feat: add textContent component

---
+++ src/components/TextContent/stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { ComponentProps } from "react";
+
+import textMock from "./mock";
+
+import TextContent from ".";
+
+export default {
+  title: "TextContent",
+  component: TextContent,
+  args: textMock,
+  parameters: {
+    backgrounds: {
+      default: "won-dark"
+    }
+  }
+} as Meta;
+
+export const Basic: Story<ComponentProps<typeof TextContent>> = ({
+  ...props
+}) => <TextContent {...props} />;
