bd5c1f705aa8d908cce04c6a914fc268016c5725
Daniel Silva <dani3l.dxp@gmail.com>
2022-04-25 19:29:41 -0300

    feat: add wishlist template

---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,55 @@
+import Base from "templates/Base";
+import { Container } from "components/Container";
+import Heading from "components/Heading";
+import GameCard, { GameCardProps } from "components/GameCard";
+import { HighlightProps } from "components/Highlight";
+import Showcase from "components/Showcase";
+import { Grid } from "components/Grid";
+import { Divider } from "components/Divider";
+import Empty from "components/Empty";
+
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps[];
+  recommendedHighlight: HighlightProps;
+  games?: GameCardProps[];
+};
+
+const Wishlist = ({
+  recommendedGames,
+  recommendedHighlight,
+  games = []
+}: WishlistTemplateProps) => {
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          Wishlist
+        </Heading>
+
+        {games.length ? (
+          <Grid>
+            {games?.map((game, index) => (
+              <GameCard key={`wishlist-${index}`} {...game} />
+            ))}
+          </Grid>
+        ) : (
+          <Empty
+            title="Your wishlist is empty"
+            description="Games added to your wishlist will appear here"
+            hasLink
+          />
+        )}
+
+        <Divider />
+      </Container>
+
+      <Showcase
+        title="You may like these games"
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  );
+};
+
+export default Wishlist;
