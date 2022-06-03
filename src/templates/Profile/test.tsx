900970679feecbb7d05fefea8071e93986c8fced
Daniel Silva <dani3l.dxp@gmail.com>
2022-06-03 10:10:10 -0300

    feat: add profile/me page and template

---
+++ src/templates/Profile/test.tsx
@@ -0,0 +1,35 @@
+import { screen } from "@testing-library/react";
+import { renderWithTheme } from "utils/tests/helpers";
+
+import Profile from ".";
+
+jest.mock("templates/Base", () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Base">{children}</div>;
+  }
+}));
+
+jest.mock("components/Heading", () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Heading">{children}</div>;
+  }
+}));
+
+jest.mock("components/ProfileMenu", () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock ProfileMenu" />;
+  }
+}));
+
+describe("<Profile />", () => {
+  it("should render the heading", () => {
+    renderWithTheme(<Profile>Lorem Ipsum</Profile>);
+
+    expect(screen.getByText("Lorem Ipsum")).toBeInTheDocument();
+    expect(screen.getByText("My profile")).toBeInTheDocument();
+    expect(screen.getByTestId("Mock ProfileMenu")).toBeInTheDocument();
+  });
+});
