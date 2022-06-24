5d8fca5b9205a61ebb9afc76b74bf4ab2dc50b0c
Daniel Silva <dani3l.dxp@gmail.com>
2022-06-24 12:18:48 -0300

    feat: add cart icon icon

---
+++ src/components/CartIcon/stories.tsx
@@ -0,0 +1,27 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { ComponentProps } from "react";
+
+import CartIcon from ".";
+
+export default {
+  title: "CartIcon",
+  component: CartIcon,
+  argTypes: {},
+  args: {
+    quantity: 3
+  },
+  parameters: {
+    layout: "centered",
+    backgrounds: {
+      default: "won-dark"
+    }
+  }
+} as Meta;
+
+export const WithItems: Story<ComponentProps<typeof CartIcon>> = ({
+  ...props
+}) => <CartIcon {...props} />;
+
+export const WithoutItems: Story<ComponentProps<typeof CartIcon>> = () => (
+  <CartIcon />
+);
