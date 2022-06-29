2614545b30437e85cd07f390f27c1f8784984ac6
Daniel Silva <dani3l.dxp@gmail.com>
2022-06-29 10:58:12 -0300

    feat: add cart dropdown component

---
+++ src/components/CartDropdown/stories.tsx
@@ -0,0 +1,34 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { ComponentProps } from "react";
+
+import CartDropdown from ".";
+
+import items from "components/CartList/mock";
+
+export default {
+  title: "CartDropdown",
+  component: CartDropdown,
+  argTypes: {},
+  args: {
+    items,
+    total: "R$300,00"
+  },
+  parameters: {
+    backgrounds: {
+      default: "won-dark"
+    }
+  },
+  decorators: [
+    (Story) => (
+      <div
+        style={{ maxWidth: "98%", display: "flex", justifyContent: "flex-end" }}
+      >
+        <Story />
+      </div>
+    )
+  ]
+} as Meta;
+
+export const Basic: Story<ComponentProps<typeof CartDropdown>> = ({
+  ...props
+}) => <CartDropdown {...props} />;
