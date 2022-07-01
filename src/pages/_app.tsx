7513463ca22ede475806332453bb3fdf80fd58d7
Daniel Silva <dani3l.dxp@gmail.com>
2022-07-01 11:32:54 -0300

    feat: add ApolloProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "hooks/useApollo";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
@@ -6,26 +8,30 @@ import GlobalStyles from "styles/global";
 import theme from "styles/theme";
 
 function App({ Component, pageProps }: AppProps) {
+  const client = useApollo(pageProps.initialApolloState);
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Wongames</title>
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          rel="shortcut icon"
-          href="/img/icon-512.png"
-          type="image/x-icon"
-        />
-        <link
-          rel="apple-touch-icon"
-          href="/img/icon-512.png"
-          type="image/x-icon"
-        />
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Wongames</title>
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="shortcut icon"
+            href="/img/icon-512.png"
+            type="image/x-icon"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/img/icon-512.png"
+            type="image/x-icon"
+          />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
