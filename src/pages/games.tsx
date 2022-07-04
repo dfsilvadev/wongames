a3d78809c934d17bd5e9783fb43f3fa51ccc7671
Daniel Silva <dani3l.dxp@gmail.com>
2022-07-04 16:54:37 -0300

    feat: populando a page games

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,17 +1,38 @@
 import GamesTemplate, { GamesTemplateProps } from "templates/Games";
 
+import { initializeApollo } from "utils/apollo";
+import { formatCurrency } from "utils/formatCurrency";
+
+import { QUERY_GAMES } from "graphql/queries/games";
 import filterItemsMock from "components/ExploreSidebar/mock";
-import gamesMock from "components/GameCardSlider/mock";
+import { queryGames, queryGamesVariables } from "graphql/generated/queryGames";
 
 export default function GamePage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />;
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo();
+
+  const { data } = await apolloClient.query<queryGames, queryGamesVariables>({
+    query: QUERY_GAMES,
+    variables: {
+      limit: 9
+    }
+  });
+
+  const games = data.games.data.map((game) => ({
+    title: game!.attributes?.name,
+    developer: game.attributes?.developers?.data[0].attributes?.name,
+    img: `http://localhost:1337${game.attributes?.cover?.data?.attributes?.url}`,
+    price: formatCurrency(game.attributes?.price)
+  }));
+
   return {
     props: {
-      games: gamesMock,
-      filterItems: filterItemsMock
+      games,
+      filterItems: filterItemsMock,
+      revalidate: 60 * 60 // 1 hours
     }
   };
 }
