9f591cf43cbf0bdbefb19c85b9a2098d30aab96e
Daniel Silva <dani3l.dxp@gmail.com>
2022-07-07 15:01:56 -0300

    feat: remove $

--- src/components/GameInfo/index.tsx
+++ src/components/GameInfo/index.tsx
@@ -20,7 +20,7 @@ const GameInfo = ({ title, description, price }: GameInfoProps) => {
         {title}
       </Heading>
 
-      <Ribbon color="secondary">{`$${price}`}</Ribbon>
+      <Ribbon color="secondary">{`${price}`}</Ribbon>
 
       <S.Description>{description}</S.Description>
 
