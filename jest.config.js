05ab29f8fd1ea3b1106b6956c5d9efde1ea7e811
Daniel Silva <dani3l.dxp@gmail.com>
2022-07-07 15:09:29 -0300

    feat: add collectCoverageFrom

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,17 @@ module.exports = {
   moduleDirectories: ["node_modules", "src"],
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts(x)?", "!src/**/stories.tsx"],
+  collectCoverageFrom: [
+    "src/**/*.ts(x)?",
+    "!src/**/stories.tsx",
+    "!src/pages/**/*.tsx",
+    "!src/styles/**/*.ts",
+    "!src/utils/apollo.ts",
+    "!src/utils/apolloCache.ts",
+    "!src/types/**/*.d.ts",
+    "!src/graphql/**/*.ts",
+    "!src/**/mock.ts"
+  ],
   setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
   modulePaths: ["<rootDir>/src", "<rootDir>/.jest"]
 };
